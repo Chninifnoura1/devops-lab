46bec23f83685d0e341e9e08b9cdf77ceee32a5e
Chninifnoura1 <noura.chninif2004@gmail.com>
2026-04-23 23:03:04 +0100

    nassimatk

--- app.py
+++ app.py
@@ -4,8 +4,35 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    #return "Version 1 - CI/CD avec Jenkins"
-    #return "Version 4 - Déployée automatiquement"
-    return "naaaaaaaaaaaaaaaaaaaaaaaa"
+    return """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Mon App</title>
+        <style>
+            body {
+                background-color: #1e1e2f;
+                color: white;
+                text-align: center;
+                font-family: Arial, sans-serif;
+            }
+            h1 {
+                color: #00ffcc;
+                font-size: 50px;
+                margin-top: 100px;
+            }
+            p {
+                color: #ffcc00;
+                font-size: 20px;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>🚀 Mon Application DevOps</h1>
+        <p>Bienvenue dans mon projet CI/CD avec Jenkins</p>
+    </body>
+    </html>
+    """
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000) 
+    app.run(host="0.0.0.0", port=5000)
