25ef5d2f269288aa135dadaa0c47ca56ee53951a
Chninifnoura1 <noura.chninif2004@gmail.com>
2026-04-23 22:57:57 +0100

    nassima

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ def home():
     #return "Version 1 - CI/CD avec Jenkins"
     #return "Version 4 - Déployée automatiquement"
     #return "VChninif noura - Déployée automatiquement"
-    return "nassima tk fffffff- Déployée automatiquement master il groupe 1"
+    return "nassima taikoukk"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000) 
