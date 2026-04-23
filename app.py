a358b2b19ea3ce089fa4a6a057faae783fde6866
Chninifnoura1 <noura.chninif2004@gmail.com>
2026-04-23 14:58:45 +0100

    new update

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ def home():
     #return "Version 1 - CI/CD avec Jenkins"
     #return "Version 4 - Déployée automatiquement"
     #return "VChninif noura - Déployée automatiquement"
-    return "Chninif noura - Déployée automatiquement"
+    return "Chninif noura - Déployée automatiquement master il"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000) 
